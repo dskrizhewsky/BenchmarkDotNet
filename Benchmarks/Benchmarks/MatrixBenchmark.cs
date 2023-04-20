e1701931cce730afa122c41fbf2d1ccb18091fe5
DESKTOP-E4S3091\dskri <dskrizhewsky@gmail.com>
2023-04-20 08:33:36 +0300

    Matrix iteration benchmarks added

---
+++ Benchmarks/Benchmarks/MatrixBenchmark.cs
@@ -0,0 +1,56 @@
+﻿#region Copyright Notice
+// ----------------------------------------------------------------------------
+// <copyright file="MatrixBenchmark.cs" company="Dmytro Skryzhevskyi">
+// </copyright>
+// ----------------------------------------------------------------------------
+#endregion
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+
+namespace Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net60)]
+[SimpleJob(RuntimeMoniker.Net70)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class MatrixBenchmark
+{
+    private int[,] _mtx;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _mtx = new int[100, 100];
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                _mtx[i, j] = i * j;
+            }
+        }
+    }
+
+    [Benchmark()]
+    public void DirectOrder()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                _mtx[i, j] = 1;
+            }
+        }
+    }
+
+    [Benchmark()]
+    public void OppositeOrder()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                _mtx[j, i] = 0;
+            }
+        }
+    }
+}
