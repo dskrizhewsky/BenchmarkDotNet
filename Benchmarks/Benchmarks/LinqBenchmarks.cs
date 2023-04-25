14b5b55ae86fa497f531450dd89efa20d89021bd
DESKTOP-E4S3091\dskri <dskrizhewsky@gmail.com>
2023-04-25 10:58:17 +0300

    Added LINQ benchmarks

---
+++ Benchmarks/Benchmarks/LinqBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+public class LinqBenchmarks
+{
+    private int[] _arr;
+    [Params(100)] public int ArrSize { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _arr = new int[ArrSize];
+        for (int i = 0; i < ArrSize - 1; i++)
+        {
+            _arr[i] = 1;
+        }
+    }
+
+    [Benchmark]
+    public int SumLinq()
+    {
+        return _arr.Sum();
+    }
+
+    [Benchmark()]
+    public int SumPure()
+    {
+        int sum = 0;
+        for (int i = 0; i < ArrSize - 1; i++)
+        {
+            sum += _arr[i];
+        }
+
+        return sum;
+    }
+}
