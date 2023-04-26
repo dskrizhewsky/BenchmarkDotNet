8029a707ae71b726baace9cf8eaf732f52c62a8c
DESKTOP-E4S3091\dskri <dskrizhewsky@gmail.com>
2023-04-26 19:07:53 +0300

    Added loggers benchmarks

---
+++ Benchmarks/Benchmarks/LoggersBenchmarks.cs
@@ -0,0 +1,62 @@
+﻿#region Copyright Notice
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 Dmytro Skryzhevskyi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+#endregion
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using log4net;
+using Serilog;
+using Serilog.Core;
+
+namespace Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net60)]
+[SimpleJob(RuntimeMoniker.Net70)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class LoggersBenchmarks
+{
+    private static readonly NLog.Logger _nlog = NLog.LogManager.GetCurrentClassLogger();
+    private readonly ILog _l4n = LogManager.GetLogger(typeof(LoggersBenchmarks));
+    private readonly Logger _slog = new LoggerConfiguration().CreateLogger();
+    private readonly string _msg = new('o', 40);
+
+    [Benchmark()]
+    public void Nlog()
+    {
+        _nlog.Info(_msg);
+    }
+
+    [Benchmark()]
+    public void L4Net()
+    {
+        _l4n.Info(_msg);
+    }
+
+    [Benchmark()]
+    public void SeriLog()
+    {
+        _slog.Information(_msg);
+    }
+}
